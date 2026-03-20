90236fe773be9cc93c36bf2e9d5a0e3acd24526b
khlud07 <khlud@knu.ua>
2026-03-20 21:47:04 +0200

    update filter
    reduced size of coefficient array, multiplication takes up less recources

--- filter/main.cpp
+++ filter/main.cpp
@@ -17,7 +17,7 @@ void fir (
   data_t x
   )
 {
-	coef_t c[N] = {53, 0, -91, 0, 313, 500, 313, 0, -91, 0,53};
+	coef_t c[N/2 + 1] = {53, 0, -91, 0, 313, 500};
 	// Write your code here
 	static
 		data_t shift_reg[N];
@@ -26,15 +26,18 @@ void fir (
 
 	acc = 0;
 	Shift_Accum_Loop:
-	for (i = N - 1; i >= 0; i--){
-		if (i == 0) {
-			acc += x * c[0];
-			shift_reg[0] = x;
-		} else {
-			shift_reg[i] = shift_reg[i - 1];
-			acc += shift_reg[i] * c[i];
-		}
-	}
+	for (i = N-1; i >= 0; i--) {
+        if (i == 0) {
+            shift_reg[0] = x;
+        } else {
+            shift_reg[i] = shift_reg[i-1];
+        }
+
+        if (i == N/2) {
+            acc += shift_reg[i] * c[i];
+        } else if (i < N/2) {
+            acc += (shift_reg[i] + shift_reg[N-1-i]) * c[i];
+        }
+    }
 	*y = acc;
 }
-
